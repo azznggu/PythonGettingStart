53e2fdd7078847a10a3a4aa68970c05532933598
azznggu <azznggu@gmail.com>
2018-10-15 17:57:53 +0900

    24.까지.

--- PythonGettingStart/list/dimension list/dimensionList.py
+++ PythonGettingStart/list/dimension list/dimensionList.py
@@ -28,7 +28,39 @@ def printDElement2(list):
         print()
 
 
+def while1(list):
+    i = 0
+    while i < len(list):
+        x,y = list[i]
+        print(x,y)
+        i += 1
 
+def while2(list):
+    i=0
+    while i < len(list):
+        j = 0
+        while j < len(list[i]):
+            print(list[i][j], end=' ')
+            j+=1
+        i+=1
+        print()
+    
+def initDimList1(outer,inner):
+    a = []
+    for i in range(outer):
+        b = []
+        for j in range(inner):
+            b.append(0)
+        a.append(b)
+    return a
+
+def initDimList2(outer,inner):
+    a = [[0 for j in range(inner)] for i in range(outer)]
+    return a
 #printDList(a)
 #printDElement(a)
 #printDElement2(sampleList)
+#while1(sampleList)
+#while2(sampleList)
+
+print(initDimList2(5,4))
